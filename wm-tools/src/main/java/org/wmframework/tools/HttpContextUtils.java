e000cf9fff8cf2545cc20991d41236a88ee89d06
wm00144 <wangmeng02@ilarge.cn>
2019-08-22 09:46:42 +0800

    新增反射工具

--- wm-tools/src/main/java/org/wmframework/tools/HttpContextUtils.java
+++ wm-tools/src/main/java/org/wmframework/tools/HttpContextUtils.java
@@ -16,4 +16,24 @@ public class HttpContextUtils {
     public static HttpServletRequest getHttpServletRequest() {
         return ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
     }
+
+    /**
+     * 获取客户端真实ip
+     *
+     * @param request request请求
+     * @return 真实IP
+     */
+    public static String getClientIp(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
 }
