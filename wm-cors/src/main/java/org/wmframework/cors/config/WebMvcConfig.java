0b36dbcc8025fb7b4fe558070055b0964f6d428a
wm00144 <wangmeng02@ilarge.cn>
2019-09-04 16:35:16 +0800

    ajax跨域cors

---
+++ wm-cors/src/main/java/org/wmframework/cors/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package org.wmframework.cors.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * cors解决ajax跨域
+ *
+ * @author: 王锰
+ * @date: 2019/9/4
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+
+}
